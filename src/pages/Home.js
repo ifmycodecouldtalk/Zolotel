8f7f3611e4d4de6e4e6a1c4119c8269fd432df78
ifmycodecouldtalk <bonsoirelliot0101@gmail.com>
2022-10-06 17:58:32 -0400

    first set for gallery

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,16 +13,16 @@ class Home extends React.Component {
         <div id="carouselExampleControls" class="carousel slide myCarousel" data-bs-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active text-center item-1">
-                    <img src='elegoo_kit.jpg' className='elegoo-img' alt='elegoo' />
-                    <h1 className='elegoo-caption'>We Sell Elegoo Kits for $39.99!</h1>
+                    <img src='elegoo_kit.jpg' className='elegoo-img downshift' alt='elegoo' />
+                    <h1 className='elegoo-caption downshift'>We Sell Elegoo Kits for $39.99!</h1>
                 </div>
                 <div class="carousel-item text-center item-2">
-                    <img src='gboy.jpg' className='gboy-img' alt='gboy' />
-                    <h1 className='elegoo-caption'>We Buy Old Electronics!</h1>
+                    <img src='gboy.jpg' className='gboy-img downshift' alt='gboy' />
+                    <h1 className='elegoo-caption downshift'>We Buy Old Electronics!</h1>
                 </div>
                 <div class="carousel-item text-center item-3">
-                    <img src='r_pi.jpg' className='r_pi-img' alt='r_pi' />
-                    <h1 className='elegoo-caption'>Raspberry Pis For Sale!</h1>
+                    <img src='r_pi.jpg' className='r_pi-img downshift' alt='r_pi' />
+                    <h1 className='elegoo-caption downshift'>Raspberry Pis For Sale!</h1>
                 </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
@@ -34,8 +34,31 @@ class Home extends React.Component {
                 <span class="visually-hidden">Next</span>
             </button>
         </div>
+        {/* Transition to Gallery with Gradient */}
+        <div className='gradient-div'></div>
         {/* Gallery */}
-
+        <div class="container">
+            <div class="row">
+                <div class="col gallery-square p-2 m-3 text-center">
+                    <h3>Shop Our Newest Gadgets!</h3>
+                    <img className='gadgets-img' alt='gadgets' src='gadgets.png' />
+                    <br />
+                    <a href='#'>Click Here to Learn More</a>
+                </div>
+                <div class="col gallery-square p-2 m-3 text-center">
+                    <h3>Tutorials, Instructions, Manuals, and More!</h3>
+                    <img className='books-img' alt='books' src='books.jpg' />
+                    <br />
+                    <a href='#'>Click Here to Learn More</a>
+                </div>
+                <div class="col gallery-square p-2 m-3 text-center">
+                    <h3>Popular, Bestsellers!</h3>
+                    <img className='ps5-img' alt='ps5' src='ps5.jpg' />
+                    <br />
+                    <a href='#'>Click Here to Learn More</a>
+                </div>
+            </div>
+        </div>
         {/* Footer */}
       </div>
     )
